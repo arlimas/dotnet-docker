420400b49253df563c0990b079e52af3f0e69de3
arlimas <alexandre.zwahlen@gmail.com>
2023-01-29 09:55:23 +0100

    Added code

---
+++ src/Models/Student.cs
@@ -0,0 +1,9 @@
+namespace myWebApp.Models;
+
+public class Student
+{
+    public int ID { get; set; }
+    public string? LastName { get; set; }
+    public string? FirstMidName { get; set; }
+    public DateTime? EnrollmentDate { get; set; }
+}
